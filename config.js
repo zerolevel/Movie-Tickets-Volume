5cfcb64efa8d2c03a5d95a24137aeeaa16ff0ef8
Mohit Daga <me@zerolevel.in>
2015-10-19 16:52:46 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+/*
+var config = {};
+
+config.updateEmails = ["update-handler@youremail.com"]; //sends updates about the shows,events scrapped
+config.errorEmails =  ["error-handler@youremail.com"]; //sends email if an error has crept in
+config.mailgunApi = "key-thisissamplekeymaynotworkgetnew"; //use mailgun api for sending email https://www.mailgun.com/
+config.senderEmail = ["from@xyz.com"]; //from email id
+module.exports = config;
+*/
